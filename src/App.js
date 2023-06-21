c321b02ce29f72ed8d9ddd33d6fd371ca33f63c1
EblissYisse <segoviavenales@hotmail.com>
2023-06-20 21:55:08 -0500

    V 0.0.1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import BasicComponent from "./components/BasicComponent";
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+//Crear una funcion que en ultima son muchos componentes
+//Como no podemos generar componentes que no devulevan valores, podemos crear funciones y arreglos para remplazar los ciclos
+//Los ciclos se deben ejecutar por fuera del elemento JSX, en funciones independientes
+//funcion push: para agregar 
+function muchosComponentes(x){
+  let arreglo=[]
+  for(let i=0; i<x;i++){
+    arreglo.push(<BasicComponent inputID={i} texto={i} key={i}/>)
+  }
+
+  return arreglo;
+}
+
+
+function App(){
+
+  //Reglas: Todas las etiquetas JSX deben tener un elemento padre
+  //        Las propiedades deben tener un valor como cadena de texto o como expresión
+  return(
+
+    //className para aplicar una hoja de estilos
+    <div className="App">         
+      {muchosComponentes(5)}
     </div>
-  );
+  )
 }
 
 export default App;
